c74fac6baebb39f66cfefeada55c40498659ca94
Stathis Sideris <stathis@pixelated-noise.com>
2015-08-22 22:19:28 +0300

    Added datomic dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.trace "0.7.8"]]
+                 [com.datomic/datomic-free "0.9.5206"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
 
