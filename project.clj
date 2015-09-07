00b05c5b2797a524f127b296140a0d3c80727801
Stathis Sideris <stathis@pixelated-noise.com>
2015-09-07 14:00:28 +0300

    Upping the version

--- project.clj
+++ project.clj
@@ -1,12 +1,15 @@
-(defproject positano "0.1.0"
+(defproject positano "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.datomic/datomic-free "0.9.5206"]
+                 [com.datomic/datomic-free "0.9.5206" :exclusions [joda-time]]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/tools.analyzer.jvm "0.6.7"]]
+
+  :pedantic? :abort
+  
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
 
