5124b167f3d8f759f0e00512fbf983be1ad74531
Stathis Sideris <stathis@pixelated-noise.com>
2015-10-10 04:32:04 +0100

    Fixed pin-point test queries but passing clojure.core/get as an input

--- test/positano/integration_test_pin_point1.clj
+++ test/positano/integration_test_pin_point1.clj
@@ -10,27 +10,27 @@
 
 ;;explore values being passed between functions using datomic queries
 
-(defn step0 [m]
+(defn fun0 [m]
   (assoc m :k0 "k0"))
-(defn step1 [m]
-  (assoc (step0 m) :k1 "k1"))
-(defn step2 [m]
-  (assoc (step1 m) :k2 "k2"))
-(defn step3 [m]
-  (let [m2 (assoc (step2 m) :k3 "k3")]
+(defn fun1 [m]
+  (assoc (fun0 m) :k1 "k1"))
+(defn fun2 [m]
+  (assoc (fun1 m) :k2 "k2"))
+(defn fun3 [m]
+  (let [m2 (assoc (fun2 m) :k3 "k3")]
     (dissoc m2 :k1)))
-(defn step4 [m]
-  (assoc (step3 m) :k4 "k4"))
-(defn step5 [m]
-  (assoc (step4 m) :k5 "k5"))
-(defn step6 [m]
-  (assoc (step5 m)
+(defn fun4 [m]
+  (assoc (fun3 m) :k4 "k4"))
+(defn fun5 [m]
+  (assoc (fun4 m) :k5 "k5"))
+(defn fun6 [m]
+  (assoc (fun5 m)
          :k6 "k6"
          :k4 "NEW VALUE"))
-(defn step7 [m]
-  (assoc (step6 m) :k7 "k7"))
+(defn fun7 [m]
+  (assoc (fun6 m) :k7 "k7"))
 
-(def funs [step7 step6 step5 step4 step3 step2 step1 step0])
+(def funs [fun7 fun6 fun5 fun4 fun3 fun2 fun1 fun0])
 
 (defn setup []
   (trace/untrace-all)
@@ -49,22 +49,23 @@
 
     (let [traced (set (map str (filter trace/traced? (trace/all-fn-vars))))]
       (is (= 8 (count traced)))
-      (is (= #{"#'positano.integration-test-pin-point1/step0"
-               "#'positano.integration-test-pin-point1/step1"
-               "#'positano.integration-test-pin-point1/step2"
-               "#'positano.integration-test-pin-point1/step3"
-               "#'positano.integration-test-pin-point1/step4"
-               "#'positano.integration-test-pin-point1/step5"
-               "#'positano.integration-test-pin-point1/step6"
-               "#'positano.integration-test-pin-point1/step7"} traced)))
+      (is (= #{"#'positano.integration-test-pin-point1/fun0"
+               "#'positano.integration-test-pin-point1/fun1"
+               "#'positano.integration-test-pin-point1/fun2"
+               "#'positano.integration-test-pin-point1/fun3"
+               "#'positano.integration-test-pin-point1/fun4"
+               "#'positano.integration-test-pin-point1/fun5"
+               "#'positano.integration-test-pin-point1/fun6"
+               "#'positano.integration-test-pin-point1/fun7"} traced)))
 
-    (step7 {:foo :bar})
+    (fun7 {:foo :bar})
     
     (is (not= :timed-out (block-until #(= 16 @db/event-counter) 10 3000)))
 
     (let [db (d/db conn)]
+      (def ddd db)
       (let [events (q/all-function-events db)
-            names ["step7" "step6" "step5" "step4" "step3" "step2" "step1" "step0"]]
+            names ["fun7" "fun6" "fun5" "fun4" "fun3" "fun2" "fun1" "fun0"]]
         (is (= 16 (count events)))
         (is (= (into #{} (for [n names
                                t [:fn-call :fn-return]]
@@ -72,14 +73,14 @@
                (->> events (map (juxt :event/fn-name :event/type)) set)))
 
         ;;test function return values
-        (is (= {"step0" {:foo :bar :k0 "k0"}
-                "step1" {:foo :bar :k0 "k0" :k1 "k1"}
-                "step2" {:foo :bar :k0 "k0" :k1 "k1" :k2 "k2"}
-                "step3" {:foo :bar :k0 "k0" :k2 "k2" :k3 "k3"}
-                "step4" {:foo :bar :k0 "k0" :k2 "k2" :k3 "k3" :k4 "k4"}
-                "step5" {:foo :bar :k0 "k0" :k2 "k2" :k3 "k3" :k4 "k4" :k5 "k5"}
-                "step6" {:foo :bar :k0 "k0" :k2 "k2" :k3 "k3" :k4 "NEW VALUE" :k5 "k5" :k6 "k6"}
-                "step7" {:foo :bar :k0 "k0" :k2 "k2" :k3 "k3" :k4 "NEW VALUE" :k5 "k5" :k6 "k6" :k7 "k7"}}
+        (is (= {"fun0" {:foo :bar :k0 "k0"}
+                "fun1" {:foo :bar :k0 "k0" :k1 "k1"}
+                "fun2" {:foo :bar :k0 "k0" :k1 "k1" :k2 "k2"}
+                "fun3" {:foo :bar :k0 "k0" :k2 "k2" :k3 "k3"}
+                "fun4" {:foo :bar :k0 "k0" :k2 "k2" :k3 "k3" :k4 "k4"}
+                "fun5" {:foo :bar :k0 "k0" :k2 "k2" :k3 "k3" :k4 "k4" :k5 "k5"}
+                "fun6" {:foo :bar :k0 "k0" :k2 "k2" :k3 "k3" :k4 "NEW VALUE" :k5 "k5" :k6 "k6"}
+                "fun7" {:foo :bar :k0 "k0" :k2 "k2" :k3 "k3" :k4 "NEW VALUE" :k5 "k5" :k6 "k6" :k7 "k7"}}
                (->> events
                     (filter q/fn-return?)
                     (map db/deserialise)
@@ -88,42 +89,40 @@
 
         ;;find which function removed :k1 from the map
         (let [res (d/q '[:find ?name2
+                         :in $ ?get
                          :where
                          [?call1   :event/fn-caller ?call2] ;;call2 calls call1
                          [?call2   :event/fn-name ?name2]
                          [?return1 :event/fn-entry ?call1] ;;return1 is the return event for call1
                          [?return2 :event/fn-entry ?call2]
-                         [?return1 :event/return-value ?val1] ;;focus on return values
-                         [?return2 :event/return-value ?val2]
+                         [?return1 :event/return-value ?v1] ;;focus on return values
+                         [?return2 :event/return-value ?v2]
                          
-                         [(read-string ?val1) ?v1] ;;deserialise
-                         [(read-string ?val2) ?v2]
-                         [(get ?v1 :k1 ::none) ?key1]
-                         [(get ?v2 :k1 ::none) ?key2]
+                         [(?get ?v1 :k1 ::none) ?key1]
+                         [(?get ?v2 :k1 ::none) ?key2]
                          [(not= ?key1 ::none)] ;;call1 had a value for key :k1
                          [(= ?key2 ::none)]    ;;call2 has no value for key :k2
-                         ] db)]
-          (is (= "step3" (ffirst res))))
+                         ] db get)]
+          (is (= "fun3" (ffirst res))))
 
         ;;discover which function changed the value of :k4
         (let [res (d/q '[:find ?name2
+                         :in $ ?get
                          :where
                          [?call1   :event/fn-caller ?call2] ;;call2 calls call1
                          [?call2   :event/fn-name ?name2]
                          [?return1 :event/fn-entry ?call1] ;;return1 is the return event for call1
                          [?return2 :event/fn-entry ?call2]
-                         [?return1 :event/return-value ?val1] ;;focus on return values
-                         [?return2 :event/return-value ?val2]
+                         [?return1 :event/return-value ?v1] ;;focus on return values
+                         [?return2 :event/return-value ?v2]
                          
-                         [(read-string ?val1) ?v1] ;;deserialise
-                         [(read-string ?val2) ?v2]
-                         [(get ?v1 :k4 ::none) ?key1]
-                         [(get ?v2 :k4 ::none) ?key2]
+                         [(?get ?v1 :k4 ::none) ?key1]
+                         [(?get ?v2 :k4 ::none) ?key2]
                          [(not= ?key1 ::none)] ;;both maps should have values for :k4
                          [(not= ?key2 ::none)]
                          [(not= ?key1 ?key2)] ;;but the values should be different
-                         ] db)]
-          (is (= "step6" (ffirst res)))
+                         ] db get)]
+          (is (= "fun6" (ffirst res)))
 ;;          (def events (map d/touch events))
           )))
     (tear-down conn)))
