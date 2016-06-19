6c104ba044ca8d4890e88e43d7e2313c6c5a5812
Stathis Sideris <stathis@pixelated-noise.com>
2016-06-19 21:42:20 +0300

    Update deps

--- project.clj
+++ project.clj
@@ -3,13 +3,12 @@
   :url "https://github.com/stathissideris/positano"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [datascript "0.13.1"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.clojure/tools.analyzer.jvm "0.6.6"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [datascript "0.15.0"]
+                 [org.clojure/core.async "0.2.385"]
+                 [org.clojure/tools.analyzer.jvm "0.6.10"]]
 
-  :pedantic? :abort
-  
+  ;;:pedantic? :abort
+
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
-
