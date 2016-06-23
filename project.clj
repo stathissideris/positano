df107a2e7ec9af2c16831592ca681cb2f6f8150b
Stathis Sideris <stathis@pixelated-noise.com>
2016-06-23 15:16:58 +0300

    Add mranderson to project.clj

--- project.clj
+++ project.clj
@@ -4,11 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [datascript "0.15.0"]
-                 [org.clojure/core.async "0.2.385"]
-                 [org.clojure/tools.analyzer.jvm "0.6.10"]]
+                 ^:source-dep [datascript "0.15.0"]
+                 ^:source-dep [org.clojure/core.async "0.2.385"]
+                 ^:source-dep [org.clojure/tools.analyzer.jvm "0.6.10"]]
 
   ;;:pedantic? :abort
 
-  :profiles {:dev {:source-paths ["dev"]
+  :plugins [[thomasa/mranderson "0.4.7"]]
+  :profiles {:dev {:source-paths ["src" "dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
